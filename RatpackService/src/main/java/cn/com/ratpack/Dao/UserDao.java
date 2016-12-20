09ec6df32a8817265f1bf11d818ca4559eceb2c8
Howell <th15817161961@gmail.com>
2016-12-20 18:39:45 +0800

    no message

--- RatpackService/src/main/java/cn/com/ratpack/Dao/UserDao.java
+++ RatpackService/src/main/java/cn/com/ratpack/Dao/UserDao.java
@@ -3,6 +3,7 @@ package cn.com.ratpack.Dao;
 import cn.com.ratpack.ModelMapper.UserMapper;
 import cn.com.ratpack.dbModel.UserModel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -26,8 +27,21 @@ public class UserDao {
         List<UserModel> list = userMapper.findAll();
         log.info("List size:"+list.size());
 
+      //  getReport((long) 1,"w","w","e");
 
         return list;
     }
+
+
+    @Cacheable(value = "reportcache", keyGenerator = "keyGenerator")
+    public String getReport(String name) {
+        System.out.println("无缓存的时候调用这里---数据库查询");
+        System.out.println("Executing DemoService" +
+                ".getHelloMessage(\"" + name + "\")");
+
+        return "Hello " + name + "!";
+    }
+
+
 
 }
--- RatpackWeb/src/main/java/cn/com/ratpack/settings/properties/AppSettings.java
+++ RatpackWeb/src/main/java/cn/com/ratpack/settings/properties/AppSettings.java
@@ -19,6 +19,19 @@ public class AppSettings {
     String mysql_aliyun_maxWait;
     String mysql_aliyun_timeBetweenEvictionRunsMillis;
 
+    String app_redis_database;
+    String app_redis_host;
+    String app_redis_password;
+    String app_redis_pool_max_active;
+    String app_redis_pool_max_idle;
+    String app_redis_pool_max_wait;
+    String app_redis_pool_min_idle;
+    int app_redis_port;
+    String app_redis_sentinel_master;
+    String app_redis_sentinel_nodes;
+    int app_redis_timeout;
+
+
     public String getMysql_aliyun() {
         return mysql_aliyun;
     }
@@ -89,5 +102,93 @@ public class AppSettings {
 
     public void setMysql_aliyun_timeBetweenEvictionRunsMillis(String mysql_aliyun_timeBetweenEvictionRunsMillis) {
         this.mysql_aliyun_timeBetweenEvictionRunsMillis = mysql_aliyun_timeBetweenEvictionRunsMillis;
+    }
+
+    public String getApp_redis_database() {
+        return app_redis_database;
+    }
+
+    public void setApp_redis_database(String app_redis_database) {
+        this.app_redis_database = app_redis_database;
+    }
+
+    public String getApp_redis_host() {
+        return app_redis_host;
+    }
+
+    public void setApp_redis_host(String app_redis_host) {
+        this.app_redis_host = app_redis_host;
+    }
+
+    public String getApp_redis_password() {
+        return app_redis_password;
+    }
+
+    public void setApp_redis_password(String app_redis_password) {
+        this.app_redis_password = app_redis_password;
+    }
+
+    public String getApp_redis_pool_max_active() {
+        return app_redis_pool_max_active;
+    }
+
+    public void setApp_redis_pool_max_active(String app_redis_pool_max_active) {
+        this.app_redis_pool_max_active = app_redis_pool_max_active;
+    }
+
+    public String getApp_redis_pool_max_idle() {
+        return app_redis_pool_max_idle;
+    }
+
+    public void setApp_redis_pool_max_idle(String app_redis_pool_max_idle) {
+        this.app_redis_pool_max_idle = app_redis_pool_max_idle;
+    }
+
+    public String getApp_redis_pool_max_wait() {
+        return app_redis_pool_max_wait;
+    }
+
+    public void setApp_redis_pool_max_wait(String app_redis_pool_max_wait) {
+        this.app_redis_pool_max_wait = app_redis_pool_max_wait;
+    }
+
+    public String getApp_redis_pool_min_idle() {
+        return app_redis_pool_min_idle;
+    }
+
+    public void setApp_redis_pool_min_idle(String app_redis_pool_min_idle) {
+        this.app_redis_pool_min_idle = app_redis_pool_min_idle;
+    }
+
+    public int getApp_redis_port() {
+        return app_redis_port;
+    }
+
+    public void setApp_redis_port(int app_redis_port) {
+        this.app_redis_port = app_redis_port;
+    }
+
+    public String getApp_redis_sentinel_master() {
+        return app_redis_sentinel_master;
+    }
+
+    public void setApp_redis_sentinel_master(String app_redis_sentinel_master) {
+        this.app_redis_sentinel_master = app_redis_sentinel_master;
+    }
+
+    public String getApp_redis_sentinel_nodes() {
+        return app_redis_sentinel_nodes;
+    }
+
+    public void setApp_redis_sentinel_nodes(String app_redis_sentinel_nodes) {
+        this.app_redis_sentinel_nodes = app_redis_sentinel_nodes;
+    }
+
+    public int getApp_redis_timeout() {
+        return app_redis_timeout;
+    }
+
+    public void setApp_redis_timeout(int app_redis_timeout) {
+        this.app_redis_timeout = app_redis_timeout;
     }
 }
